d916bb2653a1fee42224a7b4d738216e73a30f94
JavaCorn <javacorn@163.com>
2019-09-22 11:50:45 +0800

    hello

--- sysnc.sh
+++ sysnc.sh
@@ -6,6 +6,15 @@ git add .
 echo "请输入本次备注信息"
 read message
 git commit -m "$message" --no-verify
+echo "开始拉取代码……"
+git pull origin master
+
+pid=$!
+
+wait ${pid}
+
+##提交代码
+
 echo "开始提交代码……"
 git push origin master
 
@@ -13,3 +22,13 @@ pid=$!
 
 wait ${pid}
 
+echo "5s后关闭"
+sleep 1
+echo "4s后关闭"
+sleep 1
+echo "3s后关闭"
+sleep 1
+echo "2s后关闭"
+sleep 1
+echo "1s后关闭"
+sleep 1
